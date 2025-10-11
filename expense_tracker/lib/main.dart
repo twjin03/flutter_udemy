556be2eff6eee2d71acf28ec5fadaff59c4c9e9c
twjin03 <twjindev@gmail.com>
2025-10-12 02:31:51 +0900

    feat: appbar, bottomsheet로 새로운 지출 추가 기능 구현
    - AppBar()
    - showModalBottomSheet()
    - TextField()

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Expenses()));
+  runApp(MaterialApp(theme: ThemeData(useMaterial3: true), home: Expenses()));
 }
