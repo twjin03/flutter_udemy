a2414b4e5059285adc7f6ccb281a1ef4814a9400
twjin03 <twjindev@gmail.com>
2025-10-11 17:01:33 +0900

    rename: 커스텀 위젯 생성하기

--- basics/lib/main.dart
+++ basics/lib/main.dart
@@ -1,28 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.deepPurple,
-                const Color.fromARGB(255, 145, 120, 189),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(color: Colors.white, fontSize: 28),
-            ),
-          ),
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(ctx) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.deepPurple, const Color.fromARGB(255, 145, 120, 189)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
       ),
-    ),
-  );
+      child: Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
 }
