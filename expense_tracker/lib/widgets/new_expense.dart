76f74e65623f0cd84e0897e9e2fe7f9354c6851c
twjin03 <twjindev@gmail.com>
2025-10-12 02:47:18 +0900

    feat: text input 관리하기
    - controller
    - 직접 변수, 함수 설정

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -10,15 +10,40 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  // var _enteredTitle = '';
+
+  // void _saveTitleInput(String inputValue) {
+  //   _enteredTitle = inputValue;
+  // }
+
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
       child: Column(
-        children: const [
+        children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
-            decoration: InputDecoration(label: Text('Title')),
+            decoration: const InputDecoration(label: Text('Title')),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                },
+                child: const Text('Save Expense'),
+              ),
+            ],
           ),
         ],
       ),
