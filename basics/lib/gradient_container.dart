f46cfd0560d8550714bc6acc667ff66b88da66fa
twjin03 <twjindev@gmail.com>
2025-10-11 17:13:03 +0900

    refactor: 변수로 값 분리하기

--- basics/lib/gradient_container.dart
+++ basics/lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:basics/styled_text.dart';
 import 'package:flutter/material.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -10,8 +13,8 @@ class GradientContainer extends StatelessWidget {
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [Colors.deepPurple, const Color.fromARGB(255, 145, 120, 189)],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: Center(child: StyledText()),
