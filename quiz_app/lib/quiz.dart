dd566e7f9eaec1292faf575f20c7ceeac582a5bf
twjin03 <twjindev@gmail.com>
2025-10-11 18:52:16 +0900

    refactor: quiz 위젯 분리

---
+++ quiz_app/lib/quiz.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 84, 24, 189),
+                Color.fromARGB(255, 178, 147, 236),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
