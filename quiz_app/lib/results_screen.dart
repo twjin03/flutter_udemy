7b4ca96b1633324df0be3571e2ef9fd014e1cf25
twjin03 <twjindev@gmail.com>
2025-10-12 00:57:55 +0900

    feat: 퀴즈 정오 결과 화면 표시하기
    - Dart 내장 Map 기능 사용
    
    - 리스트를 map 한 이후 .toList() 사용
    - 타입 캐스팅 -> 'as' 사용해서 수행
    - .toString 사용해서 타입캐스팅

--- quiz_app/lib/results_screen.dart
+++ quiz_app/lib/results_screen.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +28,12 @@ class ResultsScreen extends StatelessWidget {
       child: Container(
         margin: EdgeInsets.all(40),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(' You answered X out of Y questions correctly!'),
             SizedBox(height: 30),
-            Text('List of answers and questions...'),
-            SizedBox(height: 30),
+            QuestionsSummary(getSummaryData()),
+            const SizedBox(height: 30),
             TextButton(onPressed: () {}, child: Text('Restart Quiz!')),
           ],
         ),
