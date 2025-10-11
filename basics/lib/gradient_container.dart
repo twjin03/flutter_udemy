a15d2049a657e15851fb892785bd1dde67007675
twjin03 <twjindev@gmail.com>
2025-10-11 17:46:55 +0900

    feat: 하나의 class에 여러 개의 constructor fn 정의하기

--- basics/lib/gradient_container.dart
+++ basics/lib/gradient_container.dart
@@ -7,6 +7,9 @@ const endAlignment = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
 
+  const GradientContainer.purple({super.key})
+    : color1 = Colors.deepPurple,
+      color2 = Colors.indigo;
   final Color color1;
   final Color color2;
 
--- basics/lib/main.dart
+++ basics/lib/main.dart
@@ -2,14 +2,5 @@ import 'package:basics/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: GradientContainer(
-          Color.fromARGB(255, 33, 17, 60),
-          Color.fromARGB(255, 168, 151, 198),
-        ),
-      ),
-    ),
-  );
+  runApp(const MaterialApp(home: Scaffold(body: GradientContainer.purple())));
 }
