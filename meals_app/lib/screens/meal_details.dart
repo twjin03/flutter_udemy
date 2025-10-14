8fd06473ff73549a9bcbb9fa3ab82de1608bf26a
twjin03 <twjindev@gmail.com>
2025-10-14 13:56:38 +0900

    feat: AnimatedSwitcher 사용해서 Implicit animation 구현

--- meals_app/lib/screens/meal_details.dart
+++ meals_app/lib/screens/meal_details.dart
@@ -21,20 +21,30 @@ class MealDetailsScreen extends ConsumerWidget {
     return Scaffold(
         appBar: AppBar(title: Text(meal.title), actions: [
           IconButton(
-            onPressed: () {
-              final wasAdded = ref
-                  .read(favoriteMealsProvider.notifier)
-                  .toggleMealFavoriteStatus(meal);
-              ScaffoldMessenger.of(context).clearSnackBars();
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text(
-                      wasAdded ? 'Meal added as a favorite.' : 'Meal removed.'),
-                ),
-              );
-            },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
-          )
+              onPressed: () {
+                final wasAdded = ref
+                    .read(favoriteMealsProvider.notifier)
+                    .toggleMealFavoriteStatus(meal);
+                ScaffoldMessenger.of(context).clearSnackBars();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(wasAdded
+                        ? 'Meal added as a favorite.'
+                        : 'Meal removed.'),
+                  ),
+                );
+              },
+              icon: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: (child, animation) {
+                  return RotationTransition(
+                    turns: Tween<double>(begin: 0.8, end: 1).animate(animation),
+                    child: child,
+                  );
+                },
+                child: Icon(isFavorite ? Icons.star : Icons.star_border,
+                    key: ValueKey(isFavorite)),
+              ))
         ]),
         body: SingleChildScrollView(
           child: Column(
