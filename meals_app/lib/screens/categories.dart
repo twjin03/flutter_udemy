be918b179da8a543e8a8a1fef5efd27c1c6da4bd
twjin03 <twjindev@gmail.com>
2025-10-14 13:38:27 +0900

    feat: Animation Controller 추가하기
    Explicit Animation 사용
    - Animation Controller 추가
    - Configure하기
    - .forward() 통해서 애니메이션 시작
    - 애니메이션 조정 (예: Padding)

--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'package:meals/data/dummy_data.dart';
-import 'package:meals/models/meal.dart';
-import 'package:meals/widgets/category_grid_item.dart';
-import 'package:meals/screens/meals.dart';
-import 'package:meals/models/category.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/models/category.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({
     super.key,
     required this.availableMeals,
@@ -14,8 +14,35 @@ class CategoriesScreen extends StatelessWidget {
 
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(microseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals
+    final filteredMeals = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
@@ -31,24 +58,32 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(24),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-      ),
-      children: [
-        // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
-        for (final category in availableCategories)
-          CategoryGridItem(
-            category: category,
-            onSelectCategory: () {
-              _selectCategory(context, category);
-            },
-          )
-      ],
-    );
+    return AnimatedBuilder(
+        animation: _animationController,
+        child: GridView(
+          padding: const EdgeInsets.all(24),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: [
+            // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+            for (final category in availableCategories)
+              CategoryGridItem(
+                category: category,
+                onSelectCategory: () {
+                  _selectCategory(context, category);
+                },
+              )
+          ],
+        ),
+        builder: (context, child) => Padding(
+              padding: EdgeInsets.only(
+                top: 100 - _animationController.value * 100,
+              ),
+              child: child,
+            ));
   }
 }
